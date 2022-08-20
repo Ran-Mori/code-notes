0d0bad09914e04871869475f55fe99cc0d77b745
izumisakai-zy <izumisakai-zy@outlook.com>
2022-08-20 13:42:41 +0800

    android:canvas

---
+++ android/Canvas/app/src/main/java/com/canvas/CanvasImageView.kt
@@ -0,0 +1,40 @@
+package com.canvas
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import android.util.TypedValue
+
+
+
+
+class CanvasImageView : AppCompatImageView {
+
+    companion object {
+        fun dp2px(context: Context, dp: Int): Float =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), context.resources.displayMetrics).toFloat()
+    }
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val point = dp2px(context, 100)
+        val radius = dp2px(context, 50)
+        val paint = Paint().apply {
+            color = resources.getColor(R.color.green, null)
+            isAntiAlias = true
+        }
+        canvas?.drawDoubleRoundRect()
+        canvas?.drawCircle(point, point, radius, paint)
+    }
+}
