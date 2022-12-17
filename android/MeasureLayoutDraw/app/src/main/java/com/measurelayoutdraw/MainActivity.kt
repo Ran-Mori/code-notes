f3bd7bb5e25e2616981082b647873faf1c451022
izumisakai-zy <izumisakai-zy@outlook.com>
2022-12-17 14:24:25 +0800

    android update 更新ViewRootImpl与measure、layout、draw的关系

--- android/MeasureLayoutDraw/app/src/main/java/com/measurelayoutdraw/MainActivity.kt
+++ android/MeasureLayoutDraw/app/src/main/java/com/measurelayoutdraw/MainActivity.kt
@@ -2,10 +2,19 @@ package com.measurelayoutdraw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var button: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        button?.setOnClickListener {
+            it.requestLayout()
+        }
     }
 }
