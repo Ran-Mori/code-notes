993c872b66e14c2f4f9cb4158a96bb539170c63f
izumisakai-zy <izumisakai-zy@outlook.com>
2023-02-04 15:33:07 +0800

    learning(andoid):学习Bitmap Drawable Canvas以及三者间的关系

---
+++ android/BitmapDrawableCanvas/app/src/main/java/com/baj/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.baj
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "IzumiSakai"
+    }
+
+    private var imageView: ImageView? = null
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.image_vew)
+        textView = findViewById(R.id.text_view)
+
+        imageView?.setOnClickListener {
+            // Bitmap -> Drawable
+//            val bitmap = BitmapFactory.decodeResource(resources, R.drawable.takagi)
+//            val bitmapDrawable = BitmapDrawable(resources, bitmap)
+//            imageView?.setImageDrawable(bitmapDrawable)
+
+            // Drawable -> Bitmap
+//            val drawable = BitmapDrawable(resources, BitmapFactory.decodeResource(resources, R.drawable.takagi))
+//            drawable.setBounds(200, 200, 1000 ,1000)
+//            val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+//            val canvas = Canvas(bitmap)
+//            drawable.draw(canvas)
+//            imageView?.setImageDrawable(BitmapDrawable(resources, bitmap))
+
+            Log.d(TAG, "image view onClick")
+        }
+
+        textView?.setOnClickListener {
+            // View -> Bitmap
+//            val bitmap = Bitmap.createBitmap(textView?.width ?: 100, textView?.height ?: 100, Bitmap.Config.ARGB_8888)
+//            val canvas = Canvas(bitmap)
+//            textView?.draw(canvas)
+
+            Log.d(TAG, "text view onClick")
+        }
+    }
+}
