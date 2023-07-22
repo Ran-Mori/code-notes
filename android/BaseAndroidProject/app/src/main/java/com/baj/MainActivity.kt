a2da5077e35bf0e53c04145b20cd5304b0b486e5
izumisakai-zy <izumisakai-zy@outlook.com>
2023-07-22 16:28:29 +0800

    update(android): 更新BaseAndroidProject初始化View方式

--- android/BaseAndroidProject/app/src/main/java/com/baj/MainActivity.kt
+++ android/BaseAndroidProject/app/src/main/java/com/baj/MainActivity.kt
@@ -12,15 +12,13 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "IzumiSakai"
     }
 
-    private var textView: TextView? = null
+    private val textView: TextView by lazy { findViewById(R.id.text_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.text_view)
-
-        textView?.setOnClickListener {
+        textView.setOnClickListener {
             Log.d(TAG, "text view onClick")
             Log.d(TAG, "brand = ${Build.BRAND}, sdk_int = ${Build.VERSION.SDK_INT}")
         }
