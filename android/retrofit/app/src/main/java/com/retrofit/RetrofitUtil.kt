bea0592976a6c0427688717e23793d7b4f31b9c1
zhengyi.sakai <zhengyi.sakai@bytedance.com>
2023-08-14 15:58:47 +0800

    learn(android): retrofit, okhttp interceptor

--- android/retrofit/app/src/main/java/com/retrofit/RetrofitUtil.kt
+++ android/retrofit/app/src/main/java/com/retrofit/RetrofitUtil.kt
@@ -1,6 +1,7 @@
 package com.retrofit
 
 import com.retrofit.api.ApiService
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
@@ -10,7 +11,9 @@ object RetrofitUtil {
     fun getClient(): ApiService {
         return Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com")
-            .client(OkHttpClient())
+            .client(OkHttpClient().apply {
+                interceptors().add(Interceptor { chain -> chain.proceed(chain.request()) })
+            })
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
