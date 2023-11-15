3a0c779f19def26a88eabe9515ee2e67f736b1d6
izumisakai-zy <izumisakai-zy@outlook.com>
2023-11-15 23:36:07 +0800

    learn(android): proguard

---
+++ android/proguard/app/src/main/java/com/proguard/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.proguard
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.annotation.Keep
+
+@Keep
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "IzumiSakai"
+    }
+
+    private val textView: TextView by lazy { findViewById(R.id.text_view) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener {
+            Log.d(TAG, "text view onClick")
+            Log.d(TAG, "brand = ${Build.BRAND}, sdk_int = ${Build.VERSION.SDK_INT}")
+        }
+    }
+}
