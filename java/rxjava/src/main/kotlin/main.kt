aa29299cdd9d9a09d581b59144ff494cee021e8f
izumisakai-zy <izumisakai-zy@outlook.com>
2024-07-13 15:03:50 +0800

    add(java): rxjava FlatMap

--- java/rxjava/src/main/kotlin/main.kt
+++ java/rxjava/src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun main() {
     testCreate()
+//    testFlatMap()
 }
 
 private fun testCreate() {
@@ -28,4 +29,41 @@ private fun testCreate() {
 
     Thread.sleep(5000)
     disposable.dispose()
+}
+
+private fun testFlatMap() {
+    Observable.create<String> {
+        println("create thread = ${Thread.currentThread().name}, time = ${System.currentTimeMillis()}")
+        Thread.sleep(2000L)
+        throw RuntimeException("") // 验证抛异常会被下面onErrorReturn给拦住
+        it.onNext("create onNext")
+    }
+        .map {
+            println("map thread = ${Thread.currentThread().name}, time = ${System.currentTimeMillis()}")
+            throw RuntimeException("") // 验证抛异常会被下面onErrorReturn给拦住
+            "${it}map"
+        }
+        .onErrorReturn {
+            println("onErrorReturn thread = ${Thread.currentThread().name}, time = ${System.currentTimeMillis()}")
+            "error"
+        }
+        .flatMap { result ->
+            Observable.create<String> {
+                println("flatMap thread = ${Thread.currentThread().name}, time = ${System.currentTimeMillis()}")
+                throw RuntimeException("") // 验证抛异常会被下面subscribe()的onError拦住
+                it.onNext("createSecond + ${result}")
+                it.onComplete()
+            }
+        }
+        .subscribeOn(Schedulers.io()) // Observable.create<String>的方法块都会在io线程执行
+        .observeOn(Schedulers.newThread()) // 下面所有的Observer都在new thread
+        .subscribe(
+            {
+                println("subscribe onNext thread = ${Thread.currentThread().name}, result = $it, time = ${System.currentTimeMillis()}")
+            },
+            {
+                println("subscribe onError thread = ${Thread.currentThread().name}, time = ${System.currentTimeMillis()}")
+                it.printStackTrace()
+            }
+        )
 }
